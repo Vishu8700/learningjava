d8bce1383f1f0a7119a165d81b25e5588e9cedeb
Vishu8700! <vg0836760@gmail.com>
2026-05-20 15:26:23 +0530

    first commit

---
+++ src/basicprograms/StudentDetails.java
@@ -0,0 +1,9 @@
+package basicprograms;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CareerCraft Technologies..............");
+        System.out.println("i am learn java");
+        System.out.println("MY NAME IS VISHAL");
+    }
+}
---
+++ src/calculatorapp/Addition.java
@@ -0,0 +1,10 @@
+package calculatorapp;
+
+public class Addition{
+    public static void main(String[] args) {
+        int a=39,b=50,c;
+        c=a+b;
+        System.out.println(c);
+    }
+
+}
