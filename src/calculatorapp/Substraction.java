b2b4b4fcf37c366a668ab64807c2f638d6a94ced
Vishu8700! <vg0836760@gmail.com>
2026-05-20 15:52:07 +0530

    add substraction program

---
+++ src/calculatorapp/Substraction.java
@@ -0,0 +1,9 @@
+package calculatorapp;
+
+public class Substraction {
+    public static void main(String[] args) {
+        int a =50,b=20,c;
+        c=a-b;
+        System.out.println(c);
+    }
+}
