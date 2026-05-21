80bd6dc974f432e036263466e79307b6ba7d77a1
Vishu8700! <vg0836760@gmail.com>
2026-05-21 12:07:31 +0530

    second commit

--- src/calculatorapp/Addition.java
+++ src/calculatorapp/Addition.java
@@ -2,7 +2,7 @@ package calculatorapp;
 
 public class Addition{
     public static void main(String[] args) {
-        int a=39,b=50,c;
+        int a=2000,b=50,c;
         c=a+b;
         System.out.println(c);
     }
